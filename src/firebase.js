72e7a6d54b2b9a9913d8f7cf725b4594b0dfa60f
Presgood16 <58942385+Presgood16@users.noreply.github.com>
2021-12-03 20:21:44 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0eBT3O9yDXPRhQ1hPrYuDbB5G-kK-k28",
-  authDomain: "clone-2aa34.firebaseapp.com",
-  projectId: "clone-2aa34",
-  storageBucket: "clone-2aa34.appspot.com",
-  messagingSenderId: "721243571813",
-  appId: "1:721243571813:web:5ec6af8dc22a39e536688c",
-  measurementId: "G-P88XJWNJ6B"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
